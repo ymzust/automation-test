eb3bf6720f91044ceb87cb312d299e5ae78bc90c
ymz <Yakov.MandezZaig@ust-global.com>
2019-11-14 11:58:25 +0200

    modeling incident_create action to ease usage

--- actions/cdc_incident_create.py
+++ actions/cdc_incident_create.py
@@ -1,8 +1,21 @@
 import cdc.sdk
 import json
+import os
 from st2common.runners.base_action import Action
 
 class IncidentCreate(Action):
-    def run(self, incident_body):
-        incident = json.loads(incident_body)
+    def run(self, name, description, priority, severity, source, type):
+        organization = 
+        incident = new_incident = cdc.model.Incident(
+            name=name,
+            description=description,
+            organization=os.getenv('CDC_ORGANIZATION_KEY'),
+            priority= priority,
+            severity=severity,
+            status="New",
+            source=source,
+            type=type,
+            tags=[],
+            playbooks=[]
+        )
         return cdc.sdk.incident.create_incident(incident)

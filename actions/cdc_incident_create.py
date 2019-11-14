eaf7dc2d0e3e12326b6c817cfd2aeb904eceb792
ymz <Yakov.MandezZaig@ust-global.com>
2019-11-14 11:37:58 +0200

    changed: incident_body from object to string

--- actions/cdc_incident_create.py
+++ actions/cdc_incident_create.py
@@ -1,6 +1,8 @@
 import cdc.sdk
+import json
 from st2common.runners.base_action import Action
 
 class IncidentCreate(Action):
     def run(self, incident_body):
-        return cdc.sdk.incident.create_incident(incident_body)
+        incident = json.loads(incident_body)
+        return cdc.sdk.incident.create_incident(incident)

0c1adb8c019e9c9dcd575d7bf14319e8d3a90dde
ymz <Yakov.MandezZaig@ust-global.com>
2019-11-14 12:27:57 +0200

    bugfixes

--- actions/cdc_incident_create.py
+++ actions/cdc_incident_create.py
@@ -5,13 +5,13 @@ from st2common.runners.base_action import Action
 class IncidentCreate(Action):
     def run(self, incident_name, incident_description, incident_priority, incident_severity, incident_source, incident_type):
         incident = cdc.model.Incident(
-            name=name,
-            description=description,
+            name=incident_name,
+            description=incident_description,
             organization=cdc.settings().organization,
-            priority= priority,
-            severity=severity,
+            priority=incident_priority,
+            severity=incident_severity,
             status="New",
-            source=source,
+            source=incident_source,
             type=incident_type,
             tags=[],
             playbooks=[]
